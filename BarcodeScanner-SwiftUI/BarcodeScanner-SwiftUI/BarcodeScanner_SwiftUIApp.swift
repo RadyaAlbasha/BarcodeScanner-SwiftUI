e12522cb9c0151ecac28ea484053e44dda104a86
RadyaAlbasha <radya.albasha15@gmail.com>
2025-06-12 19:01:24 +0300

    create new project

---
+++ BarcodeScanner-SwiftUI/BarcodeScanner-SwiftUI/BarcodeScanner_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScanner_SwiftUIApp.swift
+//  BarcodeScanner-SwiftUI
+//
+//  Created by Radya Albasha on 12/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScanner_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
